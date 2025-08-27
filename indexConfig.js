507b5c32b79355210866f7dde73bcb5d303ffb34
tejsai16@gmail.com <sai@Saitejas-MacBook-Air.local>
2025-08-27 17:08:08 +0530

    Your update message

---
+++ indexConfig.js
@@ -0,0 +1,5 @@
+// indexConfig.js
+window.indexConfig = {
+  mainHeading: "Welcome to Your Custom Dashboard!",
+  mainParagraph: "This is an example sentence that fills the page. You can edit this text in indexConfig.js to display any message you want to your users."
+};
