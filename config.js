377ebce0f82c1fad53ede4c7caf6db2e903f1a62
tejsai16@gmail.com <sai@Saitejas-MacBook-Air.local>
2025-08-27 16:30:05 +0530

    Fix tenantId in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 const azureConfig = {
-  clientId: "YOUR_CLIENT_ID_HERE",
-  tenantId: "YOUR_TENANT_ID_HERE"
+  clientId: "936c6477-5531-4ad0-99c0-62363a26b296",
+  tenantId: "5fa80da0-932d-4f40-8df7-8675b1c38ed2"
 };
